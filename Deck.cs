58623649a8ef3b902827f51926ab49d3e443985f
cechode <chunkychode@users.noreply.github.com>
2023-04-27 19:32:36 +0000

    meaningless
    Co-authored-by: ocean@chunkychode.com <ocean@chunkychode.com>

---
+++ Deck.cs
@@ -0,0 +1,15 @@
+namespace app
+{
+    public class Deck
+    {
+        Cards.Card[] _cards;
+        public void Init()
+        {
+            for (int i=0;i<52;i++) {
+                    
+            }
+        }
+        
+
+    }
+}
